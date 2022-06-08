e9ddfb6f53feea0c97163be589bbc05878ca425d
wulongfeng <wulongfeng1990@gmail.com>
2022-06-08 16:55:36 -0400

    Gift cards

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 
 This is the implementation of out paper:
+
 In this work, we propose a logical reasoning network: LogiRec to capture the asymmetric complementary relationship between products and seamlessly extend to the high-order recommendation.
 
 
---
+++ example.sh
@@ -0,0 +1,8 @@
+
+# Gift_Cards
+
+CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_valid --do_test \
+  --data_path data/Gift_Cards -n 128 -b 512 -d 400 -g 60 \
+  -lr 0.0001 --max_steps 120000 --cpu_num 6 --geo beta --valid_steps 20000 \
+  --save_checkpoint_steps 20000
+
