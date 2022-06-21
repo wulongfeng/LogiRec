e1c4d962066e469eb681cfb1fed2074e340efd9d
wulongfeng <wulongfeng1990@gmail.com>
2022-06-20 23:30:22 -0400

    minor change

--- example.sh
+++ example.sh
@@ -4,16 +4,13 @@
 CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_valid --do_test \
   --data_path data/Gift_Cards -n 128 -b 512 -d 400 -g 60 \
   -lr 0.0001 --max_steps 200000 --cpu_num 6 --valid_steps 20000 \
-  --save_checkpoint_steps 20000
 
 
-#CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_valid --do_test \
-#  --data_path data/Gift_Cards/lowOrder -n 128 -b 512 -d 400 -g 60 \
-#  -lr 0.0001 --max_steps 200000 --cpu_num 6 --valid_steps 20000 \
-#  --save_checkpoint_steps 20000
+CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_valid --do_test \
+  --data_path data/Gift_Cards/lowOrder -n 128 -b 512 -d 400 -g 60 \
+  -lr 0.0001 --max_steps 200000 --cpu_num 6 --valid_steps 20000 \
 
 
-#CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_valid --do_test \
-#  --data_path data/Gift_Cards/highOrder -n 128 -b 512 -d 400 -g 60 \
-#  -lr 0.0001 --max_steps 200000 --cpu_num 6 --valid_steps 20000 \
-#  --save_checkpoint_steps 20000
+CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_valid --do_test \
+  --data_path data/Gift_Cards/highOrder -n 128 -b 512 -d 400 -g 60 \
+  -lr 0.0001 --max_steps 200000 --cpu_num 6 --valid_steps 20000 \
